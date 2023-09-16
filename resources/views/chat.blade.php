849aa214e1430d75bf7ad6e99a90ad483104f188
Yuto Nishino <nishino@NMacBook-Air.local>
2023-09-16 21:13:27 +0900

    Initial commit

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <meta charset='utf-8' />
+</head>
+
+<body>
+    {{-- フォーム --}}
+    <form method="POST">
+        @csrf
+        <textarea rows="10" cols="50" name="sentence">{{ isset($sentence) ? $sentence : '' }}</textarea>
+        <button type="submit">ChatGPT</button>
+    </form>
+
+    {{-- 結果 --}}
+    {{ isset($chat_response) ? $chat_response : '' }}
+</body>
+
+</html>
